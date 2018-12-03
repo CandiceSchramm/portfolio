9b3d77d3b4ce1cfec34d2b083fb6ca80baee722b
candice <candiceschramm@gmail.com>
2018-12-02 21:41:54 -0700

    heroku testing

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
+const bodyParser =require("body-parser");
+
+app.use(bodyParser.json());
+// support encoded bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 
 //dynamic port binding
-const PORT = process.env.PORT || 3001;
-app.listen(PORT);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
