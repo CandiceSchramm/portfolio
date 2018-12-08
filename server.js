db30f7eb05492c53a6f98ba54178f7a144a816c6
candice <candiceschramm@gmail.com>
2018-12-08 14:20:46 -0700

    testing prod

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('/client'));
+    app.use(express.static('/client/build'));
   
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
   }
 
