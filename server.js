8ef2df3e27f41ae69c171957476695ed51f592b5
candice <candiceschramm@gmail.com>
2018-12-08 14:35:43 -0700

    testing prod

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const app = express();
-const bodyParser =require("body-parser");
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
 // support encoded bodies
 app.use(bodyParser.urlencoded({ extended: false }));
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('/client'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    app.get('*', function (req, res) {
-        const index = path.join(__dirname, 'build', 'index.html');
-        res.sendFile(index);
-      });
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("/client"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  app.get("*", function(req, res) {
+    const index = path.join(__dirname, "build", "index.html");
+    res.sendFile(index);
+  });
+}
 
 //dynamic port binding
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Express server istening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Express server istening on port ${PORT}`));
