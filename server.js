c4a25be5bc4d85628f6dc83c2be5e5c29a6b3ebf
candice <candiceschramm@gmail.com>
2018-12-08 13:46:43 -0700

    added server deliver of client build folder

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const bodyParser =require("body-parser");
 app.use(bodyParser.json());
 // support encoded bodies
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 //dynamic port binding
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Express server istening on port ${PORT}`))
